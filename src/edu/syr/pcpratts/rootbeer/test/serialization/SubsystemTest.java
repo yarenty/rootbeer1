27e7eea5abc1a66b479dec32be9a08486394c068
Phil Pratt-Szeliga <pcpratts@chirrup.org>
2012-08-12 12:57:45 -0400

    changed license to MIT license

--- src/edu/syr/pcpratts/rootbeer/test/serialization/SubsystemTest.java
+++ src/edu/syr/pcpratts/rootbeer/test/serialization/SubsystemTest.java
@@ -1,3 +1,10 @@
+/* 
+ * Copyright 2012 Phil Pratt-Szeliga and other contributors
+ * http://chirrup.org/
+ * 
+ * See the file LICENSE for copying permission.
+ */
+
 package edu.syr.pcpratts.rootbeer.test.serialization;
 
 import java.io.File;
--- src/edu/syr/pcpratts/rootbeer/testcases/rootbeertest/gpurequired/SynchronizedStaticMethodShared.java
+++ src/edu/syr/pcpratts/rootbeer/testcases/rootbeertest/gpurequired/SynchronizedStaticMethodShared.java
@@ -1,3 +1,10 @@
+/* 
+ * Copyright 2012 Phil Pratt-Szeliga and other contributors
+ * http://chirrup.org/
+ * 
+ * See the file LICENSE for copying permission.
+ */
+
 package edu.syr.pcpratts.rootbeer.testcases.rootbeertest.gpurequired;
 
 public class SynchronizedStaticMethodShared {
