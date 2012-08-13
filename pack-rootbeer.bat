d344bfc1ea72a2a0011022e60b6e8db128ece6ed
Phil Pratt-Szeliga <pcpratts@chirrup.org>
2012-08-13 03:28:29 -0400

    fixing pack scripts to work with some versions of ant

--- pack-rootbeer.bat
+++ pack-rootbeer.bat
@@ -1,2 +1,2 @@
 
-"C:\Program Files\Java\jre6\bin\java.exe" -jar lib\pack.jar -mainjar dist\Rootbeer1.jar -directory dist\lib -destjar Rootbeer.jar
+"C:\Program Files\Java\jre6\bin\java.exe" -jar lib\pack.jar -mainjar dist\Rootbeer1.jar -directory lib -destjar Rootbeer.jar
