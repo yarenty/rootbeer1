311528b72b157a24c51ab7125d7c831380d4b84c
Daniel Brown <ddb@star.sr.bham.ac.uk>
2012-08-14 14:44:16 +0100

    can now specify 'debug' as 2nd arg to batch file to create a debug version of dll

--- native/compileCudaRuntime2_win.bat
+++ native/compileCudaRuntime2_win.bat
@@ -18,16 +18,27 @@ if NOT EXIST "%JAVA_HOME%" (
 	EXIT
 )
 
+if NOT "%2"=="" (
+	if "%2"=="debug" (
+		echo Building debug version of dll...
+		set "dbg_flag=/Zi /DDEBUG=1"
+	) else (
+		set "dbg_flag=/DDEBUG=0"
+	)
+)
+
 if [%1] == [32] (
 	@call "%vcvarsall%" x86
-	cl /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /I"%CUDA_INC_PATH%" CudaRuntime2.c FastMemory.c Handles.c Cuda2DeviceMemory.c /link "%CUDA_PATH%\lib\Win32\cuda.lib" /DLL /OUT:cudaruntime_x86.dll /MACHINE:X86
+	del cudaruntime_x86.*
+	cl %dbg_flag% /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /I"%CUDA_INC_PATH%" CudaRuntime2.c FastMemory.c Handles.c Cuda2DeviceMemory.c /link "%CUDA_PATH%\lib\Win32\cuda.lib" /DLL /OUT:cudaruntime_x86.dll /MACHINE:X86
 	
 	echo Copying dll to src\edu\syr\pcpratts\rootbeer\runtime2\native\
 	move /-y .\cudaruntime_x86.dll ..\src\edu\syr\pcpratts\rootbeer\runtime2\native\
 ) else (
 	if [%1] == [64] (
 			@call "%vcvarsall%" amd64
-			cl /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /I"%CUDA_INC_PATH%" CudaRuntime2.c FastMemory.c Handles.c Cuda2DeviceMemory.c /link "%CUDA_PATH%\lib\x64\cuda.lib" /DLL /OUT:cudaruntime_x64.dll /MACHINE:X64
+			del cudaruntime_x64.*
+			cl %dbg_flag% /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /I"%CUDA_INC_PATH%" CudaRuntime2.c FastMemory.c Handles.c Cuda2DeviceMemory.c /link "%CUDA_PATH%\lib\x64\cuda.lib" /DLL /OUT:cudaruntime_x64.dll /MACHINE:X64
 			
 			echo Copying dll to src\edu\syr\pcpratts\rootbeer\runtime2\native\
 			move /-y .\cudaruntime_x64.dll ..\src\edu\syr\pcpratts\rootbeer\runtime2\native\
