ebe697364a178461b52aabb80afe3d23162dd17f
Phil Pratt-Szeliga <pcpratts@chirrup.org>
2013-02-02 15:12:18 -0500

    creating test case for ChangeThreadTest

--- src/edu/syr/pcpratts/rootbeer/test/RootbeerTestAgent.java
+++ src/edu/syr/pcpratts/rootbeer/test/RootbeerTestAgent.java
@@ -38,7 +38,11 @@ public class RootbeerTestAgent {
     if(test_case_obj instanceof TestSerialization){
       TestSerialization test_ser = (TestSerialization) test_case_obj;
       System.out.println("[TEST 1/1] "+test_ser.toString());
-      test(test_ser, true);      
+      if(test_case.equals("edu.syr.pcpratts.rootbeer.testcases.rootbeertest.gpurequired.ChangeThreadTest")){
+        testChangeThread(test_ser, true);
+      } else {
+        test(test_ser, true);
+      }
       if(m_passed){
         System.out.println("  PASSED");
         System.out.println("  Cpu time: "+m_cpuTime+" ms");
@@ -66,6 +70,7 @@ public class RootbeerTestAgent {
     LoadTestSerialization loader = new LoadTestSerialization();
     List<TestSerialization> creators = loader.load(cls_loader, "edu.syr.pcpratts.rootbeer.test.Main", run_hard_tests);
     List<TestException> ex_creators = loader.loadException(cls_loader, "edu.syr.pcpratts.rootbeer.test.ExMain");
+    List<TestSerialization> change_thread = loader.load(cls_loader, "edu.syr.pcpratts.rootbeer.test.Main", run_hard_tests);
     int num_tests = creators.size() + ex_creators.size();
     int test_num = 1;
 
@@ -94,6 +99,22 @@ public class RootbeerTestAgent {
         System.out.println("  FAILED");
         System.out.println("  "+m_message);
         m_failedTests.add(ex_creator.toString());
+      }        
+      ++test_num;
+    }
+    
+    for(TestSerialization creator : creators){
+      System.out.println("[TEST "+test_num+"/"+num_tests+"] "+creator.toString());
+      testChangeThread(creator, false);
+      ForceGC.gc();
+      if(m_passed){
+        System.out.println("  PASSED");
+        System.out.println("  Cpu time: "+m_cpuTime+" ms");
+        System.out.println("  Gpu time: "+m_gpuTime+" ms");
+      } else {
+        System.out.println("  FAILED");
+        System.out.println("  "+m_message);
+        m_failedTests.add(creator.toString());
       }        
       ++test_num;
     }
@@ -181,5 +202,75 @@ public class RootbeerTestAgent {
         m_message = "Exception is: "+ex.toString(); 
       }
     }
+  }
+
+  private void testChangeThread(TestSerialization creator, boolean print_mem) {
+    Thread t = new Thread(new ChangeThread(creator, print_mem));
+    t.start();
+    try {
+      t.join();
+    } catch(Exception ex){
+      ex.printStackTrace();
+    }
+  }
+  
+  private class ChangeThread implements Runnable {
+
+    private TestSerialization m_creator;
+    private boolean m_printMem;
+    private Rootbeer m_rootbeer;
+    
+    public ChangeThread(TestSerialization creator, boolean print_mem){
+      m_creator = creator;
+      m_printMem = print_mem;
+      m_rootbeer = new Rootbeer();
+    }
+    
+    public void run() {
+      int i = 0;
+      try {      
+        Configuration.setPrintMem(m_printMem);
+        List<Kernel> known_good_items = m_creator.create();
+        List<Kernel> testing_items = m_creator.create();
+        Stopwatch watch = new Stopwatch();
+        watch.start();
+        m_rootbeer.runAll(testing_items);
+        if(m_rootbeer.getRanGpu() == false){
+          m_message = "Ran on CPU";
+          m_passed = false;
+          return;
+        }
+        m_passed = true;
+        watch.stop();
+        m_gpuTime = watch.elapsedTimeMillis();
+        watch.start();
+        for(i = 0; i < known_good_items.size(); ++i){       
+          Kernel known_good_item = known_good_items.get(i);
+          known_good_item.gpuMethod();
+        }
+        watch.stop();
+        m_cpuTime = watch.elapsedTimeMillis();
+        for(i = 0; i < known_good_items.size(); ++i){
+          Kernel known_good_item = known_good_items.get(i);
+          Kernel testing_item = testing_items.get(i);
+          if(!m_creator.compare(known_good_item, testing_item)){
+            m_message = "Compare failed at: "+i;
+            m_passed = false;
+            return;
+          }
+        }
+      } catch(Throwable ex){
+        ex.printStackTrace(System.out);
+        m_message = "Exception thrown at index: "+i+"\n";
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        PrintWriter writer = new PrintWriter(os);
+        ex.printStackTrace(writer);
+        writer.flush();
+        writer.close();
+        m_message += os.toString();
+        m_passed = false;
+      }
+    }
+    
   }
 }
