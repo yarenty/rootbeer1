ed0bf630aa1b3be09c1bbb956449171bf566ffea
Phil Pratt-Szeliga <pcpratts@chirrup.org>
2013-02-18 05:07:46 -0500

    FastMatrix passes functional test

--- gtc2013/Matrix/src/rootbeer/examples/gtc2013/MatrixApp.java
+++ gtc2013/Matrix/src/rootbeer/examples/gtc2013/MatrixApp.java
@@ -16,14 +16,23 @@ public class MatrixApp {
   private int m_ccpu[];
   private int m_blockSize;
   private int m_gridSize;
+  private int m_blockIters;
+  private Stopwatch m_cpuWatch;
+  private Stopwatch m_gpuWatch;
 
   public MatrixApp(){
-    m_blockSize = 256;
-    m_gridSize = 256*14;
+    m_cpuWatch = new Stopwatch();
+    m_gpuWatch = new Stopwatch();
+  }
+
+  public void init(){
+    m_blockIters = 4;
+    m_blockSize = 64;
+    m_gridSize = 14;
     m_a = new int[m_blockSize*m_blockSize];
-    m_b = new int[m_blockSize*m_blockSize*m_gridSize];
-    m_ccpu = new int[m_blockSize*m_blockSize*m_gridSize];
-    m_cgpu = new int[m_blockSize*m_blockSize*m_gridSize];
+    m_b = new int[m_blockSize*m_blockSize*m_gridSize*m_blockIters];
+    m_ccpu = new int[m_blockSize*m_blockSize*m_gridSize*m_blockIters];
+    m_cgpu = new int[m_blockSize*m_blockSize*m_gridSize*m_blockIters];
 
     for(int i = 0; i < m_a.length; ++i){
       m_a[i] = 2;
@@ -36,31 +45,30 @@ public class MatrixApp {
 
   private void cpuRun(){
     int num_cores = Runtime.getRuntime().availableProcessors();
-    Stopwatch watch = new Stopwatch();
-    watch.start();
+    m_cpuWatch.start();
     List<MatrixCpuThread> threads = new ArrayList<MatrixCpuThread>();
     for(int i = 0; i < num_cores; ++i){
       MatrixCpuThread thread = new MatrixCpuThread(m_a, m_b, m_ccpu, i,
-        m_blockSize, m_gridSize, num_cores);
+        m_blockSize, m_gridSize*m_blockIters, num_cores);
       threads.add(thread);
     }
     for(int i = 0; i < num_cores; ++i){
       MatrixCpuThread thread = threads.get(i);
       thread.join();
     }
-    watch.stop();
-    System.out.println("cpu time: "+watch.elapsedTimeMillis()+" ms");
+    m_cpuWatch.stop();
+    System.out.println("avg cpu time: "+m_cpuWatch.getAverageTime()+" ms");
   }
 
   private void gpuRun(){
-    Stopwatch watch = new Stopwatch();
-    watch.start();
-    MatrixKernel matrix_kernel = new MatrixKernel(m_a, m_b, m_cgpu, m_blockSize, m_gridSize);
+    m_gpuWatch.start();
+    MatrixKernel matrix_kernel = new MatrixKernel(m_a, m_b, m_cgpu, m_blockSize, 
+      m_gridSize, m_blockIters);
     Rootbeer rootbeer = new Rootbeer();
     rootbeer.setThreadConfig(1024, 14);
     rootbeer.runAll(matrix_kernel);
-    watch.stop();
-    System.out.println("gpu time: "+watch.elapsedTimeMillis()+" ms");
+    m_gpuWatch.stop();
+    System.out.println("avg gpu time: "+m_gpuWatch.getAverageTime()+" ms");
 
     List<StatsRow> stats = rootbeer.getStats();
     for(StatsRow row : stats){
@@ -84,15 +92,19 @@ public class MatrixApp {
         System.out.println("  gpu_value: "+gpu_value);
         System.out.println("  index: "+i);
         System.exit(1);
+        return;
       }
     }
     System.out.println("Verify PASSED!");
   }
 
   public void run(){
-    cpuRun();
-    gpuRun();
-    verify();
+    for(int i = 0; i < 50; ++i){
+      init();
+      cpuRun();
+      gpuRun();
+      verify();
+    }
   }
 
   public static void main(String[] args){
--- gtc2013/Matrix/src/rootbeer/examples/gtc2013/MatrixKernel.java
+++ gtc2013/Matrix/src/rootbeer/examples/gtc2013/MatrixKernel.java
@@ -10,21 +10,24 @@ public class MatrixKernel implements Kernel {
   private int[] m_c;
   private int m_blockSize;
   private int m_gridSize;
+  private int m_blockIters;
 
-  public MatrixKernel(int[] a, int[] b, int[] c, int block_size, int grid_size){
+  public MatrixKernel(int[] a, int[] b, int[] c, int block_size, int grid_size,
+    int block_iters){
     m_a = a;
     m_b = b;
     m_c = c;
     m_blockSize = block_size;
     m_gridSize = grid_size;
+    m_blockIters = block_iters;
   }
 
   public void gpuMethod(){
 
     int block_idxx = RootbeerGpu.getBlockIdxx();
     int thread_idxx = RootbeerGpu.getThreadIdxx();
-    int bc_columns = m_blockSize * m_gridSize;
-    int a_columns = m_blockSize;
+    int bc_columns = m_blockSize * m_gridSize * m_blockIters;
+    int a_columns = m_blockSize * m_blockIters;
 
     int thread_row = thread_idxx / 32;
     int thread_col = thread_idxx % 32;
@@ -37,8 +40,9 @@ public class MatrixKernel implements Kernel {
     sub_matrix_size *= sub_matrix_size;
 
     int m_size = m_blockSize / 32;
+    int block_iters = m_blockIters;
 
-    for(int block_iter = 0; block_iter < 256; ++block_iter){ 
+    for(int block_iter = 0; block_iter < block_iters; ++block_iter){ 
       for(int sub_matrix = 0; sub_matrix < sub_matrix_size; ++sub_matrix){
         int sum = 0;
         for(int m = 0; m < m_size; ++m){
@@ -68,7 +72,7 @@ public class MatrixKernel implements Kernel {
 
           RootbeerGpu.synchthreads();
         }
-        int dest_index = (block_idxx * m_blockSize * m_blockSize) + (1024 * sub_matrix) + thread_idxx;
+        int dest_index = (block_iter * m_blockSize * m_blockSize * m_gridSize) + (block_idxx * m_blockSize * m_blockSize) + (1024 * sub_matrix) + thread_idxx;
         c[dest_index] += sum;
       }
     }
