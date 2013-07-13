824fb4ca29d1a8c9ff709e2f9b75e0e9405dc350
Phil Pratt-Szeliga <pcpratts@trifort.org>
2013-07-13 15:24:15 -0400

    commenting gtc2013/MatrixKernel

--- gtc2013/Matrix/src/rootbeer/examples/gtc2013/MatrixKernel.java
+++ gtc2013/Matrix/src/rootbeer/examples/gtc2013/MatrixKernel.java
@@ -30,16 +30,24 @@ public class MatrixKernel implements Kernel {
 
   public void gpuMethod(){
 
+    //save off fields into local variables. each read from a field hits global
+    //ram while a local variable is most likely in a register.
     int block_size = m_blockSize;
     int grid_size = m_gridSize;
     int block_iters = m_blockIters;
 
+    //getBlockIdxx is CUDA blockIdx.x
+    //getThreadIdxx is CUDA threadIdx.x
     int block_idxx = RootbeerGpu.getBlockIdxx();
     int thread_idxx = RootbeerGpu.getThreadIdxx();
 
+    //Rootbeer thread index is single dimensional right now. Convert this
+    //to a two dimensional index.
     int thread_row = thread_idxx / 32;
     int thread_col = thread_idxx % 32;
 
+    //save off the arrays into local variables. the array elements are still
+    //in global ram right now, but at least the pointers are local.
     float[] a = m_a;
     float[] b = m_b;
     float[] c = m_c;
@@ -72,41 +80,34 @@ public class MatrixKernel implements Kernel {
           float a_value = a[a_src];
           float b_value = b[b_src];
 
+          //store the a_value into shared memory at location shared_a[threadIdx.x]
+          //each thread is loading a single value of global ram into shared ram
+          //and then later in the for loop, all threads read from all values
+          //placed in shared ram. Fetches from global ram take 200-300 clock cyles
+          //while fetches from shared ram take 2-3 clock cycles. If we can have
+          //each thread fetch a single value from global memory and store all of
+          //the values into shared memory, most of the reads take 2-3 clock cycles
+          //rather than 200-300.
           RootbeerGpu.setSharedFloat(thread_idxx * 4, a_value);
+          //store the b_value into shared memory at location shared_b[threadIdx.x]
           RootbeerGpu.setSharedFloat((1024 + thread_idxx) * 4, b_value);
 
+          //sync the threads within a block
           RootbeerGpu.synchthreads();
 
+          //loop over all of shared_a[] and shared_b[]
           for(int k = 0; k < 32; ++k){
+            //read the a_value from shared_a[thread_row][k]
             a_value = RootbeerGpu.getSharedFloat((thread_row * 32 + k) * 4);
+            //read the b_value from shared_b[k][thread_col]
             b_value = RootbeerGpu.getSharedFloat((1024 + k * 32 + thread_col) * 4);
+            //multiply a_value and b_value and accumulate
             sum += a_value * b_value;
-
-            if(dest_index == 1){
-              Calculation calc = new Calculation();          
-              calc.sub_matrix_row = sub_matrix_row;
-              calc.sub_matrix_col = sub_matrix_col;
-              calc.sub_matrix = sub_matrix;
-              calc.m_size = m_size;
-              calc.thread_row = thread_row;
-              calc.thread_col = thread_col;
-              calc.dest_row = dest_row;
-              calc.dest_col = dest_col;
-              calc.block_size = block_size;
-              calc.dest_index = dest_index;
-              calc.m = m;
-              calc.k = k;
-              calc.a_src_row = a_src_row;
-              calc.a_src_col = a_src_col;
-              calc.b_src_row = b_src_row;
-              calc.b_src_col = b_src_col;
-              calc.a_value = a_value;
-              calc.b_value = b_value;
-              m_calcList.add(calc);
-            }
           }
+          //sync threads within a block
           RootbeerGpu.synchthreads();
         }
+        //increment c[dest_index] with the sum
         c[dest_index] += sum;
       }
     }
