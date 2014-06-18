e60bf4dca75c1c5f65b9dfdfc2fb44ddfa27e2fe
millecker <martin.illecker@gmail.com>
2014-06-18 10:29:32 +0200

    Update StringBuilderRunOnGpu4 test case

--- src/org/trifort/rootbeer/testcases/rootbeertest/serialization/StringBuilderRunOnGpu4.java
+++ src/org/trifort/rootbeer/testcases/rootbeertest/serialization/StringBuilderRunOnGpu4.java
@@ -12,38 +12,24 @@ import org.trifort.rootbeer.runtime.RootbeerGpu;
 
 public class StringBuilderRunOnGpu4 implements Kernel {
 
-  private double[][] m_big_array1;
-  private double[] m_array2;
-  private double[] m_array3;
+  private double[][] m_big_array;
 
-  public StringBuilderRunOnGpu4(double[][] big_array1, double[] array2,
-      double[] array3) {
-    this.m_big_array1 = big_array1;
-    this.m_array2 = array2;
-    this.m_array3 = array3;
+  public StringBuilderRunOnGpu4(double[][] big_array) {
+    this.m_big_array = big_array;
   }
 
   @Override
   public void gpuMethod() {
     if (RootbeerGpu.getThreadId() == 0) {
-      System.out.println("I will NOT throw java.lang.RuntimeException");
-
-      int x = 0;
-      // TODO Error occurs here
-      StringBuilder sb = new StringBuilder("BUT I will throw it. ");
-      // sb.append(x);
-      // System.out.println(sb.toString());
-
-      // System.out.println("BUT I will throw it. " + x);
+      // TODO CheckedFixedMemory.java:20
+      // java.lang.RuntimeException: address out of range
+      StringBuilder sb = new StringBuilder(
+          "I will throw the java.lang.RuntimeException.");
     }
 
-    // Some dummy calculations on arrays
-    int block_idxx = RootbeerGpu.getBlockIdxx();
-    int thread_idxx = RootbeerGpu.getThreadIdxx();
-    double value = m_big_array1[block_idxx][thread_idxx];
-    if (value != 0) {
-      m_array2[thread_idxx] += m_array3[thread_idxx] * value;
-    }
+    // Some dummy operation on big_array
+    m_big_array[RootbeerGpu.getBlockIdxx()][RootbeerGpu.getThreadIdxx()] = RootbeerGpu
+        .getThreadId();
   }
 
   public boolean compare(StringBuilderRunOnGpu4 rhs) {
